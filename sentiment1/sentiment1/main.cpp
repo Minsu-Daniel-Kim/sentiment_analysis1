919de69a4948bed4211fface14925cf0b19844cf
daniel <minsu.daniel.kim@gmail.com>
2016-12-16 21:04:21 -0800

    rawdata added

---
+++ sentiment1/sentiment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sentiment1
+//
+//  Created by Minsu Kim on 12/16/16.
+//  Copyright (c) 2016 Minsu Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
